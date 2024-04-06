8b77de6c69035108ca07d6b66255410467366a72
Md. Shakib Mia <smdshakibmia2001@gmail.com>
2024-04-06 20:09:35 +0600

    article image responsiveness fixed

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -6,23 +6,23 @@ import Link from 'next/link';
 const Article = ({ image, heading, date, link }) => {
   return (
     <article>
-      <div className='group'>
+      <div className='relative h-[510px] w-full'>
         <Image
           src={image}
           alt={heading.split(' ').join('-')}
-          className='!h-[510px] w-full rounded-lg transition duration-700 group-hover:grayscale '
-          width={'638'}
-          height={'510'}
+          className='rounded-lg transition duration-700 group-hover:grayscale'
+          layout='fill'
+          objectFit='cover'
         />
-        <p className='mt-2 text-base text-grey-2'>{date}</p>
-
-        <Link
-          href={link}
-          className='el-messiri mb-[1.2rem] mt-2 inline-block border-b border-[#00000030] pb-4 text-xl font-bold text-secondary md:text-2xl xl:text-[2rem]'
-        >
-          {heading}
-        </Link>
       </div>
+      <p className='mt-2 text-base text-grey-2'>{date}</p>
+
+      <Link
+        href={link}
+        className='el-messiri mb-[1.2rem] mt-2 inline-block border-b border-[#00000030] pb-4 text-xl font-bold text-secondary md:text-2xl xl:text-[2rem]'
+      >
+        {heading}
+      </Link>
       <ButtonLink link={link}>Read More</ButtonLink>
     </article>
   );
